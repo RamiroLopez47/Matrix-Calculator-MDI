d64819968794fa21e52257b005943954de7318b8
Marcelo Cattani <marcelocattani96@gmail.com>
2019-06-07 23:43:47 -0300

    Agrega Package Controller y Clase Controlador

---
+++ src/Controller/Controlador.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author CATTANI, Marcelo Omar.
+ * Todos los codigos en esta clase son de mi autoria
+ * https://github.com/marcelocattani
+ * @version 2
+ */
+public class Controlador {
+    
+}
