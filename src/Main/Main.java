c465a7ab96bc58d9d809378a76b867f7e954138c
valeguizamon <valentin.leguizamon.sevilla@gmail.com>
2019-06-11 15:29:30 -0300

    -Se agrego el paquete AnalisisDecimal y algunas de sus funciones al paquete Model -Se modifico el metodo main

---
+++ src/Main/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import Controller.Controlador;
+import View.VentanaPrincipal;
+
+/**
+ *
+ * @author user
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        Controlador controlador = new Controlador(ventana);
+        controlador.lanzar();
+    }
+    
+}
