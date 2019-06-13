558b1255ccdc7df5d31cda2806b69f525519e653
Franco Morales <francomorales145@gmail.com>
2019-06-13 13:01:17 -0300

    Se agregaron los metodos de la interfase "Validaciones" y se agrego la clase "ObjetoPersistir"

---
+++ src/Util/ObjetoPersistir.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author francomorales
+ */
+public class ObjetoPersistir implements Serializable{
+    /**
+     * Clases sujetas a modificaciones futuras
+     */
+    public static void guardarObjeto(){ }
+    public float abrirObjeto(){ return 0;}
+}
