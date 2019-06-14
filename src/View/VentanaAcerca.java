991827d9b424674ebf6bce3a83e4d262e442d9c3
AlexMarceloCastillo <alexandermarcelocastillo@gmail.com>
2019-06-13 22:55:57 -0300

    Ventana AcercaDe version2

--- src/View/VentanaAcerca.java
+++ src/View/VentanaAcerca.java
@@ -17,25 +17,41 @@ import javax.swing.*;
  * @version 1
  */
 public class VentanaAcerca extends JFrame {
+    JPanel panelPrincipal=new JPanel();
+    JLabel titulo=new JLabel();
+    JLabel version=new JLabel();
+    JLabel informacion=new JLabel();
     public VentanaAcerca(){
-        super("Acerca de MATRIX CALCULATOR");
+        setTitle("Acerca de MATRIX CALCULATOR");
         iniciarComponentes();
-        texto();
-    }
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH); //Abrir ventana maximizada
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE); //Finalizar Ejecucion al cerrarla
         
-    public void iniciarComponentes(){
-    this.setBackground(Color.DARK_GRAY);
-    this.setExtendedState(JFrame.MAXIMIZED_BOTH); //Abrir ventana maximizada
-    this.setDefaultCloseOperation(DISPOSE_ON_CLOSE); //Finalizar Ejecucion al cerrarla
-    
-     }
-    public void texto(){
-    JLabel textoAcerca=new JLabel();
-    textoAcerca.setFont(new Font("Arial", Font.BOLD, 30));
-    textoAcerca.setText("HOLA como estas");
-    textoAcerca.setSize(350,30);
-    textoAcerca.setLocation(550,40);
-        }
     }
+    public  void iniciarComponentes() {
+        this.getContentPane().add(panelPrincipal);
+        panelPrincipal.setLayout(null);
+        texto();
+        panelPrincipal.add(titulo);
+        panelPrincipal.add(version);
+        panelPrincipal.add(informacion);
+    }
+    public void texto(){
+        titulo.setText("Matrix Calculator");
+        titulo.setBounds(150,60, 400, 100);//establecer ubicacion x y z , ancho alto de la letra
+        titulo.setForeground(Color.black);//color del texto
+        titulo.setFont(new Font("Arial", Font.BOLD, 30));
+        //texto1.setOpaque(true);//activar para cambiar el fondo del texto
+        //texto1.setBackground(Color.white);//color del fondo
+        version.setText("VERSION 0.01");
+        version.setBounds(10,510,100,100);
+        version.setForeground(Color.blue);
+        informacion.setText("Matrix Calculator es una red mundial de ingenieros de software "+
+                ",artistas, escritores ");
+        informacion.setBounds(250,250,800, 20);
+        informacion.setOpaque(true);
+        informacion.setBackground(Color.white);
+    }
+ }
     
 
