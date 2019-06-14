78628e51a77ee726c9222d49ac762187b38cc9bd
Ramiro Lopez <raami1999@gmail.com>
2019-06-14 12:12:29 -0300

    Ya arranqué con lo de validaciones. Si hace falta alguna otra me avisan. Cuando podrían juntarse? Les parece mañana a la tarde/noche?

--- src/Util/Validaciones.java
+++ src/Util/Validaciones.java
@@ -10,5 +10,21 @@ package Util;
  * @author francomorales
  */
 public interface Validaciones {
-    
+
+    public boolean esCuadradaF(float[][] matriz);
+
+    public boolean sonCuadradasF(float[][] matriz, float[][] matriz2);
+
+    public boolean esCuadradaI(int[][] matriz);
+
+    public boolean sonCuadradasI(int[][] matriz, int[][] matriz2);
+
+    public boolean exponenteEntero(float[][] matriz, int exponente);
+
+    public boolean columnaIgualFila(float[][] matriz, float[][] matriz2);
+
+    public boolean mismoTamanio(float[][] matriz, float[][] matriz2);
+
+    public boolean mismoTamanio(int[][] matriz, int[][] matriz2);
+
 }
